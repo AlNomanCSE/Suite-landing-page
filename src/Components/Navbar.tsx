116631e24843303e959ce9390142e9f0d7b325eb
Abdullah Al Noman <abdullahalnomancse@gmail.com>
2024-03-02 17:52:23 +0600

    mobile nav Added

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import style from "./Navbar.module.css";
-
+import { ImCross } from "react-icons/im";
+import { IoMenu } from "react-icons/io5";
 const Navbar = () => {
+  const [isActive, setActive] = useState<boolean>(false);
+  function handleClick(): void {
+    setActive((previuous) => !previuous);
+  }
   return (
     <nav style={{ color: "#fff" }} className={style.nav}>
       <div className={style.logo}>sunnyside</div>
-      <ul className={style.manuList}>
+      {isActive ? (
+        <ImCross className={style.hamburger} onClick={handleClick} />
+      ) : (
+        <IoMenu className={style.hamburger} onClick={handleClick} />
+      )}
+      <ul className={`${style.manuList} ${isActive ? style.active : " "}`}>
         <li>About</li>
         <li>Service</li>
         <li>Projects</li>
